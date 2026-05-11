e2b01f3b49bd3b84a927bc1c23a71434534fa749
Raditya Kusuma <radityakusuma2017@gmail.com>
2026-05-11 22:11:27 +0700

    Add subscriber event handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use std::{thread, time};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        println!(
+            "In Raditya's Computer [2406365231]. Message received: {:?}",
+            message
+        );
+
+        Ok(())
+    }
+
+    fn get_handler_action(&self) -> String {
+        "user_created".to_owned()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener =
+        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+
+    _ = listener.listen(
+        "user_created".to_owned(),
+        UserCreatedHandler {},
+        crosstown_bus::QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    );
+
+    loop {}
 }
