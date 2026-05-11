5b4b19c9dd78ffb8ddf718455ad4882d6c6f4d08
Raditya Kusuma <radityakusuma2017@gmail.com>
2026-05-11 23:13:56 +0700

    Sending and processing event

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,6 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
 
         Ok(())
     }
-
-    fn get_handler_action(&self) -> String {
-        "user_created".to_owned()
-    }
 }
 
 fn main() {
